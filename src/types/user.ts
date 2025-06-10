9b78beb93c5e6e6e0080285a6707f5eaab947482
lzk12123 <3221972929@qq.com>
2025-06-10 17:06:15 +0800

    ci

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,5 +2,19 @@ export interface User {
   id: string
   username: string
   email: string
-  role: string
+  role: UserRole;
+  createdAt: string;//用户账号创立的时间
+}
+
+export enum UserRole {
+  STUDENT = 'student',
+  ADMIN = 'admin',
+}
+
+export interface UserProfile {
+  id: string;
+  realName: string;
+  university?: string;
+  major?: string;
+  graduationYear?: number;
 }
