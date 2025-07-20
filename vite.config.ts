f14f1f91a8cb273ca324210433518519c193940e
666xz666 <2965962144@qq.com>
2025-07-20 20:52:39 +0800

    [feat] remote && no dev tool

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue()
+    // vueDevTools(),
   ],
   resolve: {
     alias: {
@@ -16,6 +16,7 @@ export default defineConfig({
     },
   },
   server: {
+    host : true,
     proxy: {
       '/api': {
         target: 'http://39.107.241.92:8080', // 替换为你的后端服务地址
